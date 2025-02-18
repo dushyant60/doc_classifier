afc5781864e58e5197d6ff0c44e8cdf215d03d54
dushyant60 <dushyantsom60@gmail.com>
2025-02-18 15:40:25 +0530

    Update server.js with document classification endpoints

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 
+
 const app = express();
 const port = 8000;
+
 
 const endpoint = process.env.DOCUMENT_INTELLIGENCE_ENDPOINT;
 const key = process.env.DOCUMENT_INTELLIGENCE_API_KEY;
